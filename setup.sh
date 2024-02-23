fce2d45d30b28328c4d95733e3abf7821a60d9b3
minusno0708 <naoyao1014@gmail.com>
2024-02-24 03:24:54 +0900

    Dockerの実行権限に関する処理の順序を変更

--- setup.sh
+++ setup.sh
@@ -16,11 +16,6 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 apt update
 apt install docker-ce docker-ce-cli containerd.io
 
-# Be able to run Docker without sudo
-groupadd docker
-gpasswd -a $USER docker
-systemctl restart docker
-
 # Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
@@ -30,5 +25,11 @@ mv ./kubectl /usr/local/bin
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 install minikube-linux-amd64 /usr/local/bin/minikube
 
+# Clean up
 cd ..
 rm -rf $tmpDownloadDir
+
+# Be able to run Docker without sudo
+groupadd docker
+gpasswd -a $USER docker
+systemctl restart docker
