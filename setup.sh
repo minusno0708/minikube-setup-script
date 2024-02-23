f6a45ca8f23ff097a1fbfc8d37abcbec3bda3549
minusno0708 <naoyao1014@gmail.com>
2024-02-24 02:24:31 +0900

    Install Docker

--- setup.sh
+++ setup.sh
@@ -1,5 +1,18 @@
-# Make temporary directory for docker and minikube installation
+# Make temporary directory for Docker and minikube installation
 tmpDownloadDir="tmpMinikubeInstallDir"
 mkdir $tmpDownloadDir
+
+# Install Docker
+apt update
+apt install -y ca-certificates curl gnupg lsb-release
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+apt install docker-ce docker-ce-cli containerd.io
 
 rm -rf $tmpDownloadDir
