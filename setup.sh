a234214031890da55eb6024a996d87e3656a73ea
minusno0708 <naoyao1014@gmail.com>
2024-02-24 03:15:48 +0900

    Install minikube

--- setup.sh
+++ setup.sh
@@ -26,5 +26,9 @@ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stabl
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin
 
+# Install minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+install minikube-linux-amd64 /usr/local/bin/minikube
+
 cd ..
 rm -rf $tmpDownloadDir
